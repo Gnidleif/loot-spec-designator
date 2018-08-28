a7f9c3e07fce11ca7a5dd0c4c85a0c36dabf0eb3
Tim Fielding <gnidleif@gmail.com>
2018-08-28 18:08:45 +0200

    UI implementations started

--- Core.lua
+++ Core.lua
@@ -1,22 +1,25 @@
 ---- LOGIC SECTION
 --- Variables
-local this = CreateFrame('frame')
+local this = CreateFrame('Frame')
+local UIFrame = CreateFrame('Frame', 'LootSpecDesignator', UIParent, 'BasicFrameTemplateWithInset')
 this.specs = {} -- table containing available specs where [name] = id
 this.instances = {} -- table containing available instances where [tier][type][name] = id
 this.functions = {} -- table containing exported functions where [name] = func
 
 --- Exported functions
 --[[
-  These are all stored in the this.functions table and are called by the user through the mock "CLI"
+  These are all stored in the this.functions table and are called by the user through the mock 'CLI'
 ]]
--- functions["help"] prints a list of helpful tips on how to use the addon
-this.functions["help"] = function(args)
-    print("/lsd - brings up this help")
-    print("/lsd spec [name] - switch loot spec to [name], leave empty to match spec")
+-- functions['help'] prints a list of helpful tips on how to use the addon
+this.functions['help'] = function(args)
+    print('/lsd help - brings up this help')
+    print('/lsd spec [name] - use to switch loot spec, empty -> auto')
+    print('/lsd show - shows the main widget')
+    print('/lsd hide - hides the main widget')
 end
 
--- functions["spec"] changes the current loot spec
-this.functions["spec"] = function(args)
+-- functions['spec'] changes the current loot spec
+this.functions['spec'] = function(args)
     local name = args[2]
     if (not name) then
         SetLootSpecialization(0)
@@ -26,19 +29,20 @@ this.functions["spec"] = function(args)
     if (index) then
         SetLootSpecialization(index)
     else
-        print("invalid specialization name")
+        print('invalid specialization name')
     end
 end
 
-this.functions["get"] = function(args)
-    local id = GetLootSpecialization()
-    local name = if id == 0 then 'auto' else select(2,(GetSpecializationInfo(id))) end
-    print("Loot Specialization is set to "..name)
+this.functions['show'] = function(args)
+    UIFrame:Show()
 end
 
+this.functions['hide'] = function(args)
+    UIFrame:Hide()
+end
 --- Functions
 -- init_specializations iterate the number of available specs for the active char and adds each ID to the list of specs using the spec name as a key
-function this:init_specializations()
+function this.init_specializations()
     local i = 0
     while (i < GetNumSpecializations()) do
         local id, name = GetSpecializationInfo(i+1)
@@ -48,7 +52,7 @@ function this:init_specializations()
 end
 
 -- init_instances iterates every available instance of every available tier and adds them as lists of lists in the this.instances table 
-function this:init_instances()
+function this.init_instances()
     -- by_tier is a local function used to iterate every dungeon or raid of a given tier and return them as a table with the stored ID as a value and the name as a key
     local by_tier = function(tier, is_raid)
         -- sets the current tier to i in order to retrieve relevant instances
@@ -75,14 +79,15 @@ function this:init_instances()
     EJ_SelectTier(EJ_GetNumTiers())
 end
 
-function this:SlashCommandHandler(cmd)
+function this.SlashCommandHandler(cmd)
     if (not cmd) then
-        this.help()
+        this.functions['help']()
+        return
     end
     -- emulates a CLI by finding characters and adding them to a list of args which are then sent to the functions
-    cmd = string.lower(cmd)
+    cmd = cmd:lower()
     local args = {}
-    for w in cmd:gmatch("%S+") do
+    for w in cmd:gmatch('%S+') do
         table.insert(args, w)
     end
     -- if args[1] exists, it's always the name of the function
@@ -91,7 +96,7 @@ function this:SlashCommandHandler(cmd)
         func(args)
     else
         -- defaults to calling the help function if user tries to call non-existant function
-        this.help()
+        this.functions['help']()
     end
 end
 
@@ -100,43 +105,68 @@ end
   Checking of events should be in a descending order of commonality to do less average if-checks each time an event fires,
     this should not matter too much since if-checks are cheap and only a few events are registered to this
 ]]
-this:RegisterEvent("PLAYER_LOOT_SPEC_UPDATED")
-this:RegisterEvent("PLAYER_LOGIN")
-this:SetScript("OnEvent", function(self, event)
-    if event == "PLAYER_LOOT_SPEC_UPDATED" then
+this:RegisterEvent('PLAYER_LOOT_SPEC_UPDATED')
+this:RegisterEvent('PLAYER_LOGIN')
+this:SetScript('OnEvent', function(self, event)
+    if event == 'PLAYER_LOOT_SPEC_UPDATED' then
         local id = GetLootSpecialization()
         if (id == 0) then
-            print("Loot Specialization now matches your specialization")
+            print('Loot Specialization now matches your specialization')
         end
-    elseif event == "PLAYER_LOGIN" then
-        this.init_specializations()
-        this.init_instances()
+    elseif event == 'PLAYER_LOGIN' then
+        this:init_specializations()
+        this:init_instances()
+        this:init_UI()
+        UIFrame:Hide()
     end
 end)
 
 --- Slash recognition
-SLASH_LOOTSPECDESIGNATOR1 = "/lsd"
-SlashCmdList["LOOTSPECDESIGNATOR"] = this.SlashCommandHandler
+SLASH_LOOTSPECDESIGNATOR1 = '/lsd'
+SlashCmdList['LOOTSPECDESIGNATOR'] = this.SlashCommandHandler
 
 -- fast command to reload ui
-SLASH_RELOADUI1 = "/rl"
+SLASH_RELOADUI1 = '/rl'
 SlashCmdList.RELOADUI = ReloadUI
-
---[[
-SLASH_FRAMESTK1 = "/fs"
-SlashCmdList.FrameSTK = function()
-  LoadAddon('Blizzard_DebugTools')
-  FrameStackTooltip_Toggle()
-end
 
 -- disables turning when pressing arrow keys (used for debugging)
 for i = 1, NUM_CHAT_WINDOWS do
-  _G["ChatFrame"..i.."EditBox"]:SetAltArrowKeyMode(false)
+  _G['ChatFrame'..i..'EditBox']:SetAltArrowKeyMode(false)
 end
-]]
 
----- UI SECTION
---- Variables
---- Exported functions
---- Functions
---- Event handling
+---- USER INTERFACE SECTION
+function this.init_UI()
+    UIFrame:SetSize(300, 360)
+    UIFrame:SetPoint('CENTER', UIParent, 'CENTER')
+    UIFrame:SetMovable(true)
+    UIFrame:EnableMouse(true)
+    UIFrame:RegisterForDrag('LeftButton')
+    UIFrame:SetScript('OnDragStart', UIFrame.StartMoving)
+    UIFrame:SetScript('OnDragStop', UIFrame.StopMovingOrSizing)
+
+    UIFrame.title = UIFrame:CreateFontString(nil, 'OVERLAY')
+    UIFrame.title:SetFontObject('GameFontHighlight')
+    UIFrame.title:SetPoint('LEFT', UIFrame.TitleBg, 'LEFT', 5, 0)
+    UIFrame.title:SetText('Loot Spec Designator')
+
+    UIFrame.saveBtn = CreateFrame('Button', nil, UIFrame, 'GameMenuButtonTemplate')
+    UIFrame.saveBtn:SetPoint('CENTER', UIFrame, 'TOP', 0, -70)
+    UIFrame.saveBtn:SetSize(140, 40)
+    UIFrame.saveBtn:SetText('Save')
+    UIFrame.saveBtn:SetNormalFontObject('GameFontNormalLarge')
+    UIFrame.saveBtn:SetHighlightFontObject('GameFontHighlightLarge')
+
+    UIFrame.resetBtn = CreateFrame('Button', nil, UIFrame, 'GameMenuButtonTemplate')
+    UIFrame.resetBtn:SetPoint('TOP', UIFrame.saveBtn, 'BOTTOM', 0, -10)
+    UIFrame.resetBtn:SetSize(140, 40)
+    UIFrame.resetBtn:SetText('Reset')
+    UIFrame.resetBtn:SetNormalFontObject('GameFontNormalLarge')
+    UIFrame.resetBtn:SetHighlightFontObject('GameFontHighlightLarge')
+
+    UIFrame.loadBtn = CreateFrame('Button', nil, UIFrame, 'GameMenuButtonTemplate')
+    UIFrame.loadBtn:SetPoint('TOP', UIFrame.resetBtn, 'BOTTOM', 0, -10)
+    UIFrame.loadBtn:SetSize(140, 40)
+    UIFrame.loadBtn:SetText('Load')
+    UIFrame.loadBtn:SetNormalFontObject('GameFontNormalLarge')
+    UIFrame.loadBtn:SetHighlightFontObject('GameFontHighlightLarge')
+end
